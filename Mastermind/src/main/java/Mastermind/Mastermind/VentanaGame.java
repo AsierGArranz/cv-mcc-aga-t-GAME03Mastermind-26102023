f2de3cdae39ff5f13438d79eb1ee7ee60ddaefbc
12manel123 <manelcastellvicerezuela@gmail.com>
2023-10-26 18:44:20 +0200

    menu botones

--- Mastermind/src/main/java/Mastermind/Mastermind/VentanaGame.java
+++ Mastermind/src/main/java/Mastermind/Mastermind/VentanaGame.java
@@ -1,66 +1,151 @@
 package Mastermind.Mastermind;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
 import java.util.Random;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 public class VentanaGame extends JFrame {
-	private static final long serialVersionUID = 1L;
-	public int numBotones = 4;
-	public int numIntentos = 10;
-	
-	
-	public JButton[] seleccionColores;
-	
-	public VentanaGame() {
-		getContentPane().setLayout(null);
-		setTitle("MasterMind");
+    private static final long serialVersionUID = 1L;
+    public int numBotones = 6;
+    public int numIntentos = 10;
+    public int numSeleccion = 4;
+    public JButton[] seleccionColores;
+    private Color[] combinacionMaestra;
+    private JLabel colorSeleccionadoLabel,historial;
+    private Color[] coloresSeleccionados;
+    private int colorSeleccionadoIndex;
+    private int intentosRestantes;
+
+    public VentanaGame() {
+        getContentPane().setLayout(null);
+        setTitle("MasterMind");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
-        seleccionColores = new JButton[0];
-		
-		
-		//getContentPane().add(btnNewButton); /*<- esto lo pinta*/
-		
-		for(int i = 1; i<=numBotones; i++) {
-			JButton btnNewButton = new JButton();
-			btnNewButton.setBounds(30*i, 10, 25, 25);
-			btnNewButton.setBackground(obtenerColor(i));
-			getContentPane().add(btnNewButton); 
-			//seleccionColores[i] = btnNewButton;
-			
-		}
-		
-	}
-	
-	
-	private void seleccionarColor(int color) {
-		
-	}
-	private Color obtenerColor(int indice) {
-		switch (indice) {
-		case 1:
-			return Color.RED;
-		case 2:
-			return Color.BLUE;
-		case 3:
-			return Color.GREEN;
-		case 4:
-			return Color.YELLOW;
-		case 5:
-			return Color.ORANGE;
-		case 6:
-			return Color.PINK;
-		default:
-			return Color.GRAY;
-		}
-	}
+        setSize(400, 400);
+        seleccionColores = new JButton[numBotones];
+        coloresSeleccionados = new Color[numBotones];
+        colorSeleccionadoIndex = 0;
+        intentosRestantes = numIntentos;
+
+        colorSeleccionadoLabel = new JLabel();
+        colorSeleccionadoLabel.setBounds(10, 46, 300, 25);
+        colorSeleccionadoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        getContentPane().add(colorSeleccionadoLabel);
+
+        JButton btnBorrar = new JButton("Borrar");
+        btnBorrar.setBounds(30 * (numBotones + 1), 10, 80, 25);//Aqui se establece la posicion
+        getContentPane().add(btnBorrar);
+
+        JButton btnAdivinar = new JButton("Adivinar");
+        btnAdivinar.setBounds(294, 10, 80, 25);
+        getContentPane().add(btnAdivinar);
+
+        btnBorrar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                coloresSeleccionados = new Color[numBotones];
+                colorSeleccionadoIndex = 0;
+                actualizarEtiquetaColorSeleccionado();
+            }
+        });
+
+        combinacionMaestra = generarCombinacionMaestra();
+        System.out.println("Combinación maestra: " + Arrays.toString(combinacionMaestra));
+
+        for (int i = 0; i < numBotones; i++) {
+            JButton btnNewButton = new JButton();
+            btnNewButton.setBounds(30 * (i + 1), 10, 25, 25);
+            btnNewButton.setBackground(obtenerColor(i + 1));
+            getContentPane().add(btnNewButton);
+            seleccionColores[i] = btnNewButton;
+
+            int finalI = i;
+            btnNewButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if (colorSeleccionadoIndex < numSeleccion) {
+                        JButton botonPresionado = (JButton) e.getSource();
+                        Color colorBoton = botonPresionado.getBackground();
+                        coloresSeleccionados[colorSeleccionadoIndex] = colorBoton;
+                        colorSeleccionadoIndex++;
+                        actualizarEtiquetaColorSeleccionado();
+                    }
+                }
+            });
+        }
+        
+        btnAdivinar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (Arrays.equals(coloresSeleccionados, combinacionMaestra)) {
+                    JOptionPane.showMessageDialog(null, "¡Has adivinado la combinación!", "¡Felicidades!", JOptionPane.INFORMATION_MESSAGE);
+                } else {
+                    intentosRestantes--;
+                    if (intentosRestantes == 0) {
+                        JOptionPane.showMessageDialog(null, "¡Te has quedado sin intentos!", "Juego Terminado", JOptionPane.ERROR_MESSAGE);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Combinación incorrecta. Intentos restantes: " + intentosRestantes, "Intento Fallido", JOptionPane.WARNING_MESSAGE);
+                    }
+                }
+            }
+        });
+    }
+
+    private void actualizarEtiquetaColorSeleccionado() {
+        StringBuilder colorText = new StringBuilder("<html>Colores seleccionados: ");
+        for (Color color : coloresSeleccionados) {
+            if (color != null) {
+                colorText.append("<span style='background-color: rgb(").append(color.getRed()).append(",").append(color.getGreen()).append(",").append(color.getBlue()).append("); width: 25px; height: 25px;'>&nbsp;&nbsp;</span>");
+            }
+        }
+        colorText.append("</html>");
+        colorSeleccionadoLabel.setText(colorText.toString());
+    }
+
+    private Color obtenerColor(int indice) {//Aqui se almacena los colores posibles
+        switch (indice) {
+            case 1:
+                return Color.RED;
+            case 2:
+                return Color.BLUE;
+            case 3:
+                return Color.GREEN;
+            case 4:
+                return Color.YELLOW;
+            case 5:
+                return Color.ORANGE;
+            case 6:
+                return Color.PINK;
+            case 7:
+                return Color.BLACK;
+            case 8:
+                return Color.CYAN;
+            case 9:
+                return Color.MAGENTA;
+            case 10:
+                return Color.DARK_GRAY;
+            default:
+                return Color.GRAY;
+        }
+    }
+
+    private Color[] generarCombinacionMaestra() {
+        Random random = new Random();
+        Color[] combinacion = new Color[numSeleccion];
+        for (int i = 0; i < numSeleccion; i++) {
+            int colorIndex = random.nextInt(numBotones);
+            combinacion[i] = obtenerColor(colorIndex + 1);
+        }
+        return combinacion;
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    VentanaGame frame = new VentanaGame();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 }
