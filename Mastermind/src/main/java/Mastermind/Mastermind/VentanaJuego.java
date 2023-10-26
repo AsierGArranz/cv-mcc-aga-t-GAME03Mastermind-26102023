a05adfce0d6eff365f96019d33457cff36f7944b
12manel123 <manelcastellvicerezuela@gmail.com>
2023-10-26 16:39:23 +0200

    Codigo base

---
+++ Mastermind/src/main/java/Mastermind/Mastermind/VentanaJuego.java
@@ -0,0 +1,113 @@
+package Mastermind.Mastermind;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class VentanaJuego extends JFrame {
+    private int numColores = 6;
+    private int numFichas = 4;  
+    private int intentosMaximos = 10; 
+
+    private int[] codigoSecreto;
+    private int numIntentos;
+    private int intentosRestantes;
+
+    private JButton[] seleccionColores;
+    private JButton botonComprobar;
+    private JLabel resultadoLabel;
+    private JPanel panelJuego;
+    private JPanel panelSeleccion;
+
+    public VentanaJuego() {
+        numIntentos = 0;
+        intentosRestantes = intentosMaximos;
+
+        // Genera el código secreto
+        codigoSecreto = generarCodigoSecreto();
+
+        setTitle("MasterMind");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+
+        panelJuego = new JPanel();
+        panelSeleccion = new JPanel();
+        resultadoLabel = new JLabel("");
+        botonComprobar = new JButton("Comprobar");
+
+        seleccionColores = new JButton[numColores];
+
+        for (int i = 0; i < numColores; i++) {
+            seleccionColores[i] = new JButton();
+            seleccionColores[i].setBackground(obtenerColor(i));
+            final int colorSeleccionado = i;
+            seleccionColores[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    seleccionarColor(colorSeleccionado);
+                }
+            });
+            panelSeleccion.add(seleccionColores[i]);
+        }
+
+        botonComprobar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                comprobarIntento();
+            }
+        });
+
+        panelJuego.add(resultadoLabel);
+        panelJuego.add(botonComprobar);
+
+        setLayout(new BorderLayout());
+        add(panelSeleccion, BorderLayout.NORTH);
+        add(panelJuego, BorderLayout.CENTER);
+
+        reiniciarJuego();
+    }
+
+    private int[] generarCodigoSecreto() {
+        Random random = new Random();
+        int[] codigo = new int[numFichas];
+
+        for (int i = 0; i < numFichas; i++) {
+            codigo[i] = random.nextInt(numColores);
+        }
+
+        return codigo;
+    }
+
+    private Color obtenerColor(int indice) {
+        switch (indice) {
+            case 0:
+                return Color.RED;
+            case 1:
+                return Color.BLUE;
+            case 2:
+                return Color.GREEN;
+            case 3:
+                return Color.YELLOW;
+            case 4:
+                return Color.ORANGE;
+            case 5:
+                return Color.PINK;
+            default:
+                return Color.GRAY;
+        }
+    }
+
+    private void seleccionarColor(int color) {
+    	
+    }
+
+    private void comprobarIntento() {
+    }
+
+    private void reiniciarJuego() {
+        numIntentos = 0;
+        intentosRestantes = intentosMaximos;
+        resultadoLabel.setText("");
+    }
+}
